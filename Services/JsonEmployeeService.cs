3b651cd8874c717e1e4d6eb67b73f07a67112c46
Alvee Hassan Akash <alvee@ualberta.ca>
2021-04-18 17:01:43 -0600

    * dummy_employee.json: added dummy json objects(employee) as   database. Service added for the dummy employee.
    * Employee.cs:
    * JsonEmployeeService.cs:

---
+++ Services/JsonEmployeeService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Microsoft.AspNetCore.Hosting;
+using web_api.Models;
+
+namespace web_api.Services
+{
+    public class JsonEmployeeService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public JsonEmployeeService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.ContentRootPath, "Database", "dummy_employee.json"); }
+        }
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            using var jsonFileReader = File.OpenText(JsonFileName);
+            return JsonSerializer.Deserialize<Employee[]>(jsonFileReader.ReadToEnd(),
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+        }
+    }
+}
