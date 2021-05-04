c902bbc3efb414458863a16a68cc810e019cbcec
Alvee Hassan Akash <alvee@ualberta.ca>
2021-05-03 22:02:32 -0600

    added react bootstrap with temp navbar comp

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ In the project directory, you can run:
 
 ### `npm start`
 
+package.json changed to "start": "BROWSER=google-chrome react-scripts start",
+for development purpose
+
+npx used to create the application
+
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import Body from "./components/Body";
-import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 process.title = "react_ui";
 function App() {
   return (
-    <div className="container">
-      <Header />
-      <Body />
+    <div className="container-fluid">
+      <Navbar />
     </div>
   );
 }
