0ca0855b4f6cca32c341a82850b0dd6311bbd4a5
Alvee Hassan Akash <alvee@ualberta.ca>
2021-05-09 00:35:49 -0600

    few components added. also basic routing added. needs to implement webapi, login, auth, redirect, etc.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import Navigation from "./components/Navigation";
-import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import About from "./components/About";
+import Favourite from "./components/Favourite";
+import Experience from "./components/Experience";
+import Example from "./components/Example";
 
 process.title = "react_ui";
 function App() {
   return (
     <React.Fragment>
-      <Navigation />
-      <div className="container-fluid"></div>
+      <Router>
+        <Header />
+        <Container fluid={true}>
+          <Switch>
+            <Route exact path="/" component={About} />
+            <Route path="/about" component={About} />
+            <Route path="/favourite" component={Favourite} />
+            <Route path="/experience" component={Experience} />
+            <Route path="/example" component={Example} />
+          </Switch>
+        </Container>
+      </Router>
     </React.Fragment>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types"; // impt
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+
+const spaceBetweenNavItems = "mx-2";
 
 export class Header extends Component {
   constructor(props) {
     super(props);
-
     this.state = {};
   }
 
   // using default props, if parent comp does not provide any values.
   static defaultProps = {
-    title: "Welcome",
+    title: "User!",
   };
 
   render() {
@@ -18,14 +20,42 @@ export class Header extends Component {
     console.log(this.props);
     return (
       <header>
-        <h1>{title} to React UI</h1>
-        <a
-          href="https://alvee.azurewebsites.net"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Azure web app
-        </a>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="/">Alvee Akash</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="about" className={spaceBetweenNavItems}>
+                About
+              </Nav.Link>
+              <Nav.Link href="favourite" className={spaceBetweenNavItems}>
+                Favourite
+              </Nav.Link>
+            </Nav>
+            <Nav className="ml-auto">
+              <Nav.Link href="experience" className={spaceBetweenNavItems}>
+                Experience
+              </Nav.Link>
+              <Nav.Link href="example" className={spaceBetweenNavItems}>
+                Example
+              </Nav.Link>
+              <NavDropdown title="In Progress" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Navbar.Text className={spaceBetweenNavItems}>{title}</Navbar.Text>
+          </Navbar.Collapse>
+        </Navbar>
       </header>
     );
   }
